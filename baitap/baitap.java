a7f711f166068e9d2343d436a5cacd0f1e1b33bd
Thien Vu <vtranthien99@gmail.com>
2026-07-08 20:10:20 +0700

    bài hello

---
+++ baitap/baitap.java
@@ -0,0 +1,13 @@
+package baitap;
+
+import java.util.Scanner;
+
+public class baitap {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+         String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
